68efdee8490eef7f07679ec8d2ae7eae894e785d
qingxuan <2022810469@qq.com>
2025-01-14 20:02:28 +0800

    2025114

---
+++ py学习代码/20241221/基础语法.py
@@ -0,0 +1,12 @@
+str='123456789'
+print(str)
+print(str[0:-1])
+print(str[0:9:2])
+print('-------------')
+print('qwe\nqwe')
+
+print(r'qwe\nqwe')
+# !/usr/bin/python3
+
+for i in range(19):
+    print(i)
